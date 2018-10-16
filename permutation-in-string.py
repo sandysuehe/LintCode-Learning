10fea879b5ac044863183cd831cbe6a3799f23e8
He Xue <hexue@4paradigm.com>
2018-10-16 17:46:52 +0800

    update

---
+++ permutation-in-string.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Author: (sandysuehe@gmail.com)
+#########################################################################
+# Created Time: 2018-10-16 15:42:06
+# File Name: permutation-in-string.py
+# Description: 
+#########################################################################
+def checkInclusion(s1, s2):
+    if not s1 or not s2:
+        return False
+
+    hash_map = {}
+    for char in s1:
+        hash_map[char] = hash_map.get(char, 0) + 1
+    
+    count = len(hash_map)
+    n1 = len(s1)
+    n2 = len(s2)
+
+    start = 0
+    end = 0
+
+    # sliding window
+    while end < n2:
+        end_char = s2[end]
+        if end_char in hash_map:
+            # s2中的字符end_char也在在s1, hash_map[end_char]--
+            hash_map[end_char] = hash_map[end_char] - 1
+
+            if hash_map.get(end_char) == 0:
+                # hash_map中的end_char已被使用完
+                count -= 1
+        end += 1
+
+        while count == 0:
+            if end - start == n1:
+                return True
+            # 如果start->end长度不等于n1，即需要移动窗口左边界
+            start_char = s2[start]
+            if start_char in hash_map:
+                hash_map[start_char] = hash_map[start_char] + 1
+                if hash_map.get(start_char) > 0:
+                    count += 1
+            start += 1
+
+    return False
+
+s1 = "ab"
+s2 = "eidbaooo"
+s2 = "eidboaoo"
+print checkInclusion(s1, s2)
+# vim: set noexpandtab ts=4 sts=4 sw=4 :
---
+++ removeComments.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Author: (sandysuehe@gmail.com)
+#########################################################################
+# Created Time: 2018-10-16 16:44:34
+# File Name: removeComments.py
+# Description: 
+#########################################################################
+def removeComments1(source):
+    if not source:
+        return []
+
+    ans = []
+    out = ""
+    iscomment = False
+
+    for i in range(0, len(source)):
+        j = 0
+        while j < len(source[i]):
+            if not iscomment and j+1 < len(source[i]) and source[i][j] == "/" and source[i][j+1] == "/":
+                # 如果没有注释，并且遇到了//的话，那么s相当于没有内容
+                break
+            elif not iscomment and j+1 < len(source[i]) and source[i][j] == "/" and source[i][j+1] == "*":
+                # 如果没有注释，并且遇到了/*就说明有注释，直到下一个*/为止 
+                iscomment = True
+                j += 1
+            elif iscomment and j+1 < len(source[i]) and source[i][j] == "*" and source[i][j+1] == "/":
+                # 如果还在注释的情况下，遇到*/，那么就说明结束注释 
+                iscomment = False
+                j += 1
+            elif not iscomment:
+                out = out + source[i][j]
+            j += 1
+
+        if not iscomment and len(out):
+            ans.append(out)
+            out = ""
+    return ans
+
+
+source = ["a/*comment", "line", "more_comment*/b"]
+source = ["/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"]
+print removeComments1(source)
+# vim: set noexpandtab ts=4 sts=4 sw=4 :
