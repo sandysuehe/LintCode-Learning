e654d033a39f60b5b7e72a08b041386b17d1ef1f
He Xue <hexue@4paradigm.com>
2018-10-23 20:03:21 +0800

    a

---
+++ StringProcess/string-replace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Author: (sandysuehe@gmail.com)
+#########################################################################
+# Created Time: 2018-10-23 18:47:47
+# File Name: string-replace.py
+# Description: 
+#########################################################################
+def stringReplace(a, b, s):
+    # Write your code here
+
+A = ["ab","aba"]
+B = ["cc","ccc"]
+S = "ababa"
+print stringReplace(A, B, C)
+
+A = ["ab","aba"]
+B = ["cc","ccc"]
+S = "aaaaa"
+print stringReplace(A, B, C)
+
+A = ["cd","dab","ab"]
+B = ["cc","aaa","dd"]
+S = "cdab"
+print stringReplace(A, B, C)
+# vim: set noexpandtab ts=4 sts=4 sw=4 :
